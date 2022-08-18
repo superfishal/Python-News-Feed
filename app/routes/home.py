697f0cc041a3784ca385c3ea3c1f95f74a54281a
superfishal <97133716+superfishal@users.noreply.github.com>
2022-08-17 17:13:04 -0700

    login/signup features work, need to render dashboard on redirect not working

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -15,12 +15,17 @@ def index():
              .all())
     return render_template(
         'homepage.html',
-        posts=posts)
+        posts=posts,
+        loggedIn=session.get('loggedIn')
+    )
 
 
 @bp.route('/login')
 def login():
-    return render_template('login.html')
+    # not logged in yet
+    if session.get('loggedIn') is None:
+        return render_template('login.html')
+    return redirect('/dashboard')
 
 
 @bp.route('/post/<id>')
@@ -28,4 +33,8 @@ def single(id):
     # get single post by id
     db = get_db()
     post = db.query(Post).filter(Post.id == id).one()
-    return render_template('single-post.html', post=post)
+    return render_template(
+        'single-post.html',
+        post=post,
+        loggedIn=session.get('loggedIn')
+    )
